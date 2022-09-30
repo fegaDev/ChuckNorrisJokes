c2866fe71e992a9f0ca913d7b86dc21a212df0df
fegaDev <fegadeveloper@gmail.com>
2022-09-30 10:06:48 -0500

    chuckNorrisJokes!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useEffect, useState} from 'react'
 function App() {
+  const [chiste, setChiste] = useState('')
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      'X-RapidAPI-Key': '52db18f06fmsh5a6ae844bcfa7f2p1bb427jsn58ee8e4234ef',
+      'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com'
+    }
+  };
+
+  const cambiar = () =>{
+    fetch('https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random', options)
+    .then(response => response.json())
+    .then(response => setChiste(response.value))
+    .catch(err => console.error(err));
+  }
+
+  useEffect(() => {
+    cambiar()
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {chiste}
+      <button onClick={cambiar}>Jokes</button>
     </div>
   );
 }
-
 export default App;
